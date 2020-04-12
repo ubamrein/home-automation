8ae19ed2cd75591c8d92e629d006ea91487b04a7
amrein <amrein@ubique.ch>
2020-04-12 13:13:54 +0200

    fix timeoit problems

--- HomeAutomation/Models/RadioService.cs
+++ HomeAutomation/Models/RadioService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ namespace HomeAutomation.Models
 {
     public class RadioService : Service
     {
-        public List<string> RadioStations {get;set;}
+        public List<string> RadioStations {get;set;} = new List<string>();
         
         public async Task LoadStations() {
             var client = new HttpClient();
@@ -19,20 +20,28 @@ namespace HomeAutomation.Models
        
 
         private async Task<List<string>> LoadNextStations(string url = null) {
-            List<string> tmpStations = new List<string>();
-            var client = new HttpClient();
-            string action = url == null ? $"{this.BaseUrl}/RadioBrowse?service={this.Name}" : $"{this.BaseUrl}/RadioBrowse?service={this.Name}&url={url}";
-            var response = await client.GetAsync(action);
-            XDocument root = XDocument.Parse(await response.Content.ReadAsStringAsync());
-            foreach(var ele in root.Descendants()) {
-                 if(ele.Name == "item" && ele.Attribute("type")?.Value == "audio") {
-                    tmpStations.Add(ele.Attribute("url").Value);
+            try {
+                List<string> tmpStations = new List<string>();
+                var client = new HttpClient();
+                string action = url == null ? $"{this.BaseUrl}/RadioBrowse?service={this.Name}" : $"{this.BaseUrl}/RadioBrowse?service={this.Name}&url={url}";
+                var response = await client.GetAsync(action);
+                XDocument root = XDocument.Parse(await response.Content.ReadAsStringAsync());
+                foreach(var ele in root.Descendants()) {
+                    if(ele.Name == "item" && ele.Attribute("type")?.Value == "audio") {
+                        tmpStations.Add(ele.Attribute("URL").Value);
+                    }
+                    else if(ele.Name == "item" && ele.Attribute("type")?.Value == "link") {
+                        tmpStations.AddRange(await LoadNextStations(ele.Attribute("URL").Value));
+                    }
+                    if(tmpStations.Count > 10) {
+                        return tmpStations;
+                    }
                 }
-                else if(ele.Name == "item" && ele.Attribute("type")?.Value == "link") {
-                    tmpStations.AddRange(await LoadNextStations(ele.Attribute("url").Value));
-                }
+                return tmpStations;
             }
-            return tmpStations;
+            catch(Exception ex) {
+                return new List<string>();
+            }
         }
     }
 }
