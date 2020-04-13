db4540da509d1e5eb646736bbca1109e1150443d
Patrick Amrein <amrein@ubique.ch>
2020-04-13 14:24:23 +0200

    handle errors

--- HomeAutomation/Controllers/ApiController.cs
+++ HomeAutomation/Controllers/ApiController.cs
@@ -37,13 +37,14 @@ namespace HomeAutomation.Controllers
             var resp = await client.GetAsync($"{baseurl}/{action}");
             XDocument doc = XDocument.Parse(await resp.Content.ReadAsStringAsync());
             var status = doc.Element("status");
+
             return new Status() {
                 Artist = status.Element("artist")?.Value,
                 Album = status.Element("album")?.Value,
-                Db = double.Parse(status.Element("db")?.Value),
-                Position = int.Parse(status.Element("secs")?.Value),
+                Db = status.Element("db") == null? 0 : double.Parse(status.Element("db").Value),
+                Position = status.Element("secs") == null? 0 : int.Parse(status.Element("secs").Value),
                 Title = status.Element("title1")?.Value + "\n" + status.Element("title2")?.Value + "\n" + status.Element("title3")?.Value,
-                Volume = int.Parse(status.Element("volume")?.Value),
+                Volume = status.Element("volume") == null? 0 : int.Parse(status.Element("volume").Value),
                 Image = status.Element("image")?.Value
             };
         }
